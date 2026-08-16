0efbbb062bb3dc8675ede331a75d05a5eb80f203
yuenck-source <yuenck@web.wfjlps.edu.hk>
2026-08-16 16:44:40 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 // config.js - 全站集中設定檔
 
 const CONFIG = {
+
   MAIN_SHEET_ID: '1q3NLPoGG8qr33knDt6REdLM8-GVrarzSercPLrjKSbo',
   DAILY_SHEET_ID: '1KAVUnTWYN0BhrZsUWwCI8AP9aDHfNYAe_TQ7Ab1Ulmk',
+  SECRET_KEY: "wfjlps.edu.hk_0716",
 
   // 請確認這一行已存在且正確填入 Sheet ID
   IDEAS_SHEET_ID: '1Zgyq8EG-sbkVbQmi-YKYB0q2xOhfsEqL6MNtlqZ8f-0', 
@@ -15,7 +17,7 @@ const CONFIG = {
   },
 
   API_URLS: {
-    DAILY: 'https://script.google.com/macros/s/AKfycby7--etiRb80LznLs1boPcx8pjEVjmiZbLMIM0UC3JGgP8FCpLNc7tkhrCbCciFWDgnqQ/exec',
+    DAILY: 'https://script.google.com/macros/s/AKfycbxQ3pvI0K5q4pUeZ458FufaKj6QsPbUKYmGY7GNUJKjTP5khGAlBjAzj6eQtTKT0Bl7lA/exec',
     IDEAS: 'https://script.google.com/macros/s/AKfycbzQcRU-AsQb8oY5Wb5n8XOtZGIWkp8FXxCJLd_NrUqrBNqQmHl8ZdPvd2ABVa9KaJmk/exec',
     HABIT: 'https://script.google.com/macros/s/AKfycbz72-QL780wO-NVLsszPusQN3WGqCCeb-vn0yZEI8I6dgGwFaawpJtbUATozrE631rE/exec'
   }
